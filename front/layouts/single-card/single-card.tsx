b780115eba2ee337a6bac2cbcefa84e9d36edfa8
aoticombr <krobrelus@gmail.com>
2023-09-19 23:18:02 -0400

    Update single-card.tsx

--- front/layouts/single-card/single-card.tsx
+++ front/layouts/single-card/single-card.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ScrollView from 'devextreme-react/scroll-view';
 import './single-card.scss';
-import type { SingleCardProps } from '../../types';
 
+export interface SingleCardProps {
+    title?: string;
+    description?: string;
+}
 export default function SingleCard({ title, description, children }: React.PropsWithChildren<SingleCardProps>) {
   return (
     <ScrollView height={'100%'} width={'100%'} className={'with-footer single-card'}>
