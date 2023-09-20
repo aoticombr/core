b444520704a5b8d476d25ebf344086032854e42c
krobrelus <paulo@aoti.com.br>
2023-09-19 21:29:40 -0400

    core front

---
+++ front/app-navigation.tsx
@@ -0,0 +1,208 @@
+export const navigation = [
+  {
+    text: 'Home',
+    path: '/',
+    icon: 'home'
+  },
+{
+        text: 'Config. Sistema',
+        icon: 'columnproperties',
+    items: [
+{
+        text: 'Usuários',
+        path: '/USER',
+},
+{
+        text: 'Grupo de Empresas',
+        path: '/EMPRESAS',
+},
+{
+        text: 'Usuario x Grupo',
+        path: '/USEREMP',
+},
+    ]
+},
+{
+        text: 'Entidades',
+        icon: 'add',
+    items: [
+{
+        text: 'Clientes',
+        path: '/CLI00001',
+},
+{
+        text: 'Profissão',
+        path: '/CLI00010',
+},
+{
+        text: 'Paises',
+        path: '/CLI00009',
+},
+{
+        text: 'Nacionalidades',
+        path: '/CLI00008',
+},
+{
+        text: 'Estado Civil',
+        path: '/CLI00007',
+},
+{
+        text: 'Cidade',
+        path: '/EVENT0011',
+},
+    ]
+},
+{
+        text: 'Config. Imobilizado',
+        icon: 'car',
+    items: [
+{
+        text: 'Veiculos',
+        path: '/IMOB0010',
+},
+{
+        text: 'Combustivel',
+        path: '/IMOB0004',
+},
+{
+        text: 'Equipamentos',
+        path: '/IMOB0001',
+},
+{
+        text: 'Categoria de Equipamentos',
+        path: '/IMOB0002',
+},
+{
+        text: 'Situação de Equipamentos/Veiculos',
+        path: '/IMOB0003',
+},
+{
+        text: 'Tipo de Instrumentos',
+        path: '/EVENT0002',
+},
+    ]
+},
+{
+        text: 'Config. Eventos',
+        icon: 'event',
+    items: [
+{
+        text: 'Locais de Eventos',
+        path: '/EVENT0001',
+},
+{
+        text: 'Tom',
+        path: '/EVENT0006',
+},
+{
+        text: 'Tipo de Eventos',
+        path: '/EVENT0004',
+},
+{
+        text: 'Funções',
+        path: '/EVENT0007',
+},
+{
+        text: 'Tipo Repertorio',
+        path: '/EVENT0008',
+},
+{
+        text: 'Sonorização',
+        path: '/EVENT0013',
+},
+{
+        text: 'Momento',
+        path: '/EVENT0014',
+},
+{
+        text: 'Status da Proposta',
+        path: '/EVENT0015',
+},
+{
+        text: 'Transporte',
+        path: '/EVENT0010',
+},
+{
+        text: 'Forma de Pagamento',
+        path: '/EVENT0017',
+},
+    ]
+},
+{
+        text: 'Config. Produtos',
+        icon: 'product',
+    items: [
+{
+        text: 'Serviço',
+        path: '/PROD0012',
+},
+{
+        text: 'Marca',
+        path: '/PROD0002',
+},
+{
+        text: 'Unidade',
+        path: '/PROD0001',
+},
+{
+        text: 'Modelo',
+        path: '/PROD0003',
+},
+{
+        text: 'Grupo',
+        path: '/PROD0005',
+},
+{
+        text: 'Sub Grupo',
+        path: '/PROD0006',
+},
+{
+        text: 'Produtos',
+        path: '/PROD0011',
+},
+{
+        text: 'Categoria',
+        path: '/PROD0004',
+},
+{
+        text: 'Cor',
+        path: '/PROD0008',
+},
+    ]
+},
+{
+        text: 'Config. Financeiro',
+        icon: 'formula',
+    items: [
+{
+        text: 'Moeda',
+        path: '/FIN0002',
+},
+{
+        text: 'Banco',
+        path: '/FIN0001',
+},
+    ]
+},
+{
+        text: 'Config. Comercial',
+        icon: 'money',
+    items: [
+{
+        text: 'Tabela de Preço',
+        path: '/PROD0007',
+},
+    ]
+},
+{
+        text: 'Comercial',
+        icon: 'money',
+    items: [
+{
+        text: 'Proposta',
+        path: '/EVENT0003',
+},
+    ]
+},
+  ];
+
