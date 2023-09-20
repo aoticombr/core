fdc528490e92d28b52d659874bb1c1c0012935a6
aoticombr <krobrelus@gmail.com>
2023-09-19 23:19:31 -0400

    Update side-nav-outer-toolbar.tsx

--- front/layouts/side-nav-outer-toolbar/side-nav-outer-toolbar.tsx
+++ front/layouts/side-nav-outer-toolbar/side-nav-outer-toolbar.tsx
@@ -9,8 +9,10 @@ import { Template } from 'devextreme-react/core/template';
 import { useMenuPatch } from 'core/front/utils/patches';
 import { ButtonTypes } from 'devextreme-react/button';
 import { TreeViewTypes } from 'devextreme-react/tree-view';
-import type { SideNavToolbarProps } from 'core/front/types';
 
+export interface SideNavToolbarProps {
+    title: string;
+}
 export default function SideNavOuterToolbar({ title, children }: React.PropsWithChildren<SideNavToolbarProps>) {
   const scrollViewRef = useRef<ScrollView>(null);
   const navigate = useNavigate();
